bf5f22568afc70ca52b59f2e752762cc1d69513a
Javi <105263003+Javierasalazar1@users.noreply.github.com>
2022-08-05 12:19:53 -0400

    Add files via upload
    mejorar fecha de termino e inicio en editar reserva (aun con errores)

--- crud/edit.php
+++ crud/edit.php
@@ -25,33 +25,39 @@
         $fechater = $_POST['Fecha_Termino'];
 
                 /* separar fecha*/    
-    $f=explode('T',$fechaini);
-    $y=$f[0]."-".$f[1];
-    $fi=explode('-',$y);
-    $fii=$fi[2]."-".$fi[1]."-".$fi[0]."-".$fi[3];
-    $fa=explode(':',$fii);
-       /* separar fecha*/ 
-    $s=explode('T',$fechater);
-    $t=$s[0]."-".$s[1];
-    $ff=explode('-',$t);
-    $fff=$ff[2]."-".$ff[1]."-".$ff[0]."-".$ff[3];
-    $fb=explode(':',$fff);
-
-    if($fa[0]>$fb[0]){
-        die("la fecha de inicio debe ser menor a la de término");
-    }else{
-        if($fa[1]>$fb[1]){
-          die("el mes de inicio debe ser menor o igual a el mes de termino");
-        }else{
-            if($fa[2]>$fb[2]){
-                die("el dia de inicio debe ser menor o igual a el dia de termino");
-            }else{
-                if($fa[3]>$fb[3]){
-                    die("la hora de inicio debe ser menor a la hora de termino");
-               }
-            }
-        }
-    }
+                $f=explode('T',$fechaini);
+                $y=$f[0]."-".$f[1];
+                $fi=explode('-',$y);
+                $fii=$fi[2]."-".$fi[1]."-".$fi[0]."-".$fi[3];
+                $fiii=explode(':',$fii);
+                $fiiii=$fiii[0]."-".$fiii[1]."-".$fiii[2]."-".$fiii[3]."-".$fiii[4];
+                $fa=explode('-',$fiiii);
+                   /* separar fecha*/ 
+                $s=explode('T',$fechater);
+                $t=$s[0]."-".$s[1];
+                $ff=explode('-',$t);
+                $fff=$ff[2]."-".$ff[1]."-".$ff[0]."-".$ff[3];
+                $ffff=explode(':',$fff);
+                $fffff=$ffff[0]."-".$ffff[1]."-".$ffff[2]."-".$ffff[3]."-".$ffff[4];
+                $fb=explode('-',$fffff);
+            
+               
+                if($fa[0]>$fb[0]){
+                    if($fa[1]==$fb[1]){
+                        die("el dia de inicio debe ser menor o igual a el dia de termino");
+                        }
+                    }else{
+                          if($fa[1]>$fb[1]){
+                                die("el mes de inicio debe ser menor o igual a el dia de termino");
+                                }
+                        else{
+                                if($fa[0]==$fb[0]){
+                                 if($fa[3]>$fb[3]){
+                                 die("la hora de inicio debe ser menor a la hora de termino");
+                               }
+                        }
+                    }}
+                
 
 
         $update=mysqli_query($con,"UPDATE reserva set cod_espacioC='$espac', Fecha_inicio='$fechaini', Fecha_fin='$fechater'WHERE id=$id");
